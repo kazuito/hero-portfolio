b9d49d1b34a082b0488084d01c1442419bae2a7d
Kazuma Ito <zarigani2x@gmail.com>
2025-05-16 01:26:03 +0900

    use cn for className on body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { cn } from "@heroui/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn("antialiased", geistSans.variable, geistMono.variable)}
       >
         <Providers>{children}</Providers>
       </body>
