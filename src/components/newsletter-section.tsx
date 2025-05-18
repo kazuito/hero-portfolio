5fb2e60dd7d2804e1fe9386bf0b4fcb621680053
Kazuma Ito <zarigani2x@gmail.com>
2025-05-18 18:25:46 +0900

    show toast on subscribed

--- src/components/newsletter-section.tsx
+++ src/components/newsletter-section.tsx
@@ -1,20 +1,64 @@
-import { Button, Input } from "@heroui/react";
+import { addToast, Button, Input, Spinner } from "@heroui/react";
 import Section from "./section";
+import { useState } from "react";
+import { ForwardIcon } from "lucide-react";
 
 const NewsletterSection = () => {
+  const [sending, setSending] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleSend = () => {
+    setSending(true);
+    setTimeout(() => {
+      setSending(false);
+      setEmail("");
+      addToast({
+        title: "Subscribed successfully. Enjoy!",
+        color: "success",
+        timeout: 3000,
+        classNames: {
+          title: "text-foreground",
+          base: "bg-background border-none",
+          icon: "text-primary",
+        },
+      });
+    }, 2000);
+  };
+
   return (
     <Section heading="Newsletter">
-      <div className="">
-        <Input size="sm" label="Email" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSend();
+        }}
+      >
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          isDisabled={sending}
+          size="sm"
+          label="Email"
+        />
         <div className="mt-4 flex items-center justify-end gap-4">
           <p className="text-default-500 text-sm/tight">
             My knowledge about product design, once a month. No spam.
           </p>
-          <Button size="sm" color="primary" variant="flat" className="shrink-0">
+          <Button
+            type="submit"
+            size="sm"
+            color="primary"
+            variant="flat"
+            className="shrink-0"
+            isDisabled={sending}
+            endContent={
+              sending && <Spinner variant="dots" size="sm" color="primary" />
+            }
+          >
             Subscribe
           </Button>
         </div>
-      </div>
+      </form>
     </Section>
   );
 };
